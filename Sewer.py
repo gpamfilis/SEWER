ef131e588310d4a15e5ffd75ff9f2a5b79f29821
George Pamfilis <gpamfilis@gmail.com>
2015-06-13 16:52:27 +0300

    renamed variables

--- Sewer.py
+++ Sewer.py
@@ -5,6 +5,7 @@ __contact__ = 'gpamfilis@gmail.com'
 
 import pandas as pd
 from sewer_calculations import *
+from law_guidelines import *
 data = pd.read_excel('sewer_data.xlsx', sheetname='data-sewer')
 
 
@@ -19,26 +20,30 @@ class SewerDesign:
         self.n0 = 0.015
         pass
 
-    def type_one(self, flow_rate, slope, fullness_ratio):
+    def type_one(self, flow_rate, slope, fullness_ratio, network_type='pantoroiko'):
         """
         :param flow_rate: flow rate [m^3/sec]
         :param slope: slope [m/km]
         :param fullness_ratio: fullness ratio
         :return:
         """
+        flow_rate /= 1000.
+        print '[1.T1] Q is: {} [m^3/sec]'.format(flow_rate)
+        diameter_min = 0.4
         # step 1 full pipe flow
         flow_full_pipe = flow_rate / flow_ratio_calculation(fullness_ratio)
-        print 'Q0 is: {} [m^3/sec]'.format(flow_full_pipe)
+        print '[1.T1] Q0 is: {} [m^3/sec]'.format(flow_full_pipe)
         pipe_diameter = diameter_from_available(pipe_diameter_calculation(self.n0, flow_full_pipe, slope))
-        if pipe_diameter < 0.4:
-            print 'needs larger diameter', pipe_diameter
-            dia = input('new diameter [m]: ')
-            self.type_two(flow_rate, slope, dia)
+        print '[2.T1] The pipe diameter is: {} [m]'.format(pipe_diameter)
+        if pipe_diameter < diameter_min:
+            print '[3.T1] needs diameter >= {} [m]'.format(diameter_min)
+            new_diameter = input('new diameter? (start with 0.4 [m]): ')
+            self.type_two(flow_rate, slope, new_diameter)
         else:
             print 'diameter is ok: ', pipe_diameter
-            dia = input('new diameter [m]: ')
-
-            self.type_three(flow_rate, slope, dia)
+            #new_diameter = input('new diameter [m]: ')
+            print '[4.T1] Proceeding for TYPE-3 hydraulics problem (check whether the diameter is fit for our case'
+            self.type_three(flow_rate, slope, pipe_diameter)
 
     def type_two(self, flow_rate, slope, pipe_diameter):
         #step 1
@@ -82,11 +87,11 @@ if __name__ == '__main__':
         for i, d in enumerate(data['Q']):
             print 'SECTION: ', data['Section'][i]
             print ''
-            sd.type_one(d/1000., data['S'][i], 0.5)
+            sd.type_one(d/1000., data['S'][i], 0.5)  # divide with 1000 to convert to m^3/sec
             print ''
     else:
 
-        q = input('enter flow rate [m^3/s]: ')
+        q = input('enter flow rate [L/s]: ')
         s = input('enter slope [-]: ')
         y_d = input('enter fullness ratio y/D [-]: ')
-        sd.type_one(q/1000., s, y_d)
+        sd.type_one(q, s, y_d)
